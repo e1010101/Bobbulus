568039bc7c72cc96d313ed620861f8b7e3d8f3a9
e1010101 <ezratio15195@gmail.com>
2023-08-10 16:32:41 +0800

    Add rudimentary min-max algorithm

--- AI.cpp
+++ AI.cpp
@@ -21,26 +21,36 @@ Move findRandomMove(vector<Move> validMoves) {
 Move findBestMove(GameState myState, vector<Move> validMoves) {
   int turnMultiplier = myState.colorToMove == 0 ? 1 : -1;
   int bestMoveIndex = 0;
-  int bestMoveScore = -CHECKMATE;
+  int opponentMinMaxScore = CHECKMATE;
   int currentScore = 0;
   for (int i = 0; i < validMoves.size(); i++) {
     GameState newState = myState;
     newState.makeMove(validMoves[i]);
-    if (newState.isCheckmate()) {
-      currentScore = CHECKMATE;
-    } else if (newState.isStalemate()) {
-      currentScore = STALEMATE;
-    } else {
-      currentScore = evaluateGameState(newState) * turnMultiplier;
+    vector<Move> opponentMoves = newState.getValidMoves();
+    int opponentMaxScore = -CHECKMATE;
+    for (int j = 0; j < opponentMoves.size(); j++) {
+      GameState newState2 = newState;
+      newState2.makeMove(opponentMoves[j]);
+      if (newState2.isCheckmate()) {
+        currentScore = -turnMultiplier * CHECKMATE;
+      } else if (newState2.isStalemate()) {
+        currentScore = STALEMATE;
+      } else {
+        currentScore = -turnMultiplier * evaluateGameState(newState2);
+      }
+
+      if (currentScore > opponentMaxScore) {
+        opponentMaxScore = currentScore;
+      }
     }
 
-    if (currentScore > bestMoveScore) {
-      bestMoveScore = currentScore;
+    if (opponentMaxScore < opponentMinMaxScore) {
+      opponentMinMaxScore = opponentMaxScore;
       bestMoveIndex = i;
     }
   }
   cout << "Best move: " << validMoves[bestMoveIndex].getChessNotation() << endl;
-  cout << "Score: " << bestMoveScore << endl;
+  cout << "Score: " << opponentMinMaxScore << endl;
   return validMoves[bestMoveIndex];
 }
 
